6a8e3622fbbefc5ddcabf7e661f4aab8f18ebd57
Max Boyko <maxboykomax@gmail.com>
2021-07-04 23:57:46 -0700

    Begin automating the provisioning of key azure resources

---
+++ resources/variables.tf
@@ -0,0 +1,10 @@
+
+variable "prefix" {
+    description = "The prefix used for all resources in this project"
+    default = "azure-cms-app"
+}
+
+variable "location" {
+    description = "The location of the resources in this project"
+    default = "West US"
+}

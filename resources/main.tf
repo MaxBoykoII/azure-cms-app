4f8dc170f5723a3673650259c09780303595ca28
Max Boyko <maxboykomax@gmail.com>
2021-07-05 11:28:12 -0700

    Extend terraform configuration to include a firewall rule for local development

--- resources/main.tf
+++ resources/main.tf
@@ -1,3 +1,6 @@
+# To run the Terraform config on localhost with secrets:
+# terraform plan -var-file="secrets.tfvars"
+
 provider "azurerm" {
   version = "=2.20.0"
   features {}
@@ -6,4 +9,32 @@ provider "azurerm" {
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
+}
+
+resource "azurerm_mysql_server" "database" {
+    name = var.db_name
+    location = var.location
+    resource_group_name = azurerm_resource_group.main.name
+
+    administrator_login = "mysqladmin"
+    administrator_login_password = var.password
+
+    sku_name = "B_Gen5_1"
+    storage_mb = 5120
+    version = "8.0"
+
+    auto_grow_enabled = false
+    backup_retention_days = 7
+    geo_redundant_backup_enabled = false
+    infrastructure_encryption_enabled = false
+    public_network_access_enabled = true
+    ssl_enforcement_enabled = false
+}
+
+resource "azurerm_mysql_firewall_rule" "firewall_rule" {
+  name = "office"
+  resource_group_name = azurerm_mysql_server.database.resource_group_name
+  server_name = azurerm_mysql_server.database.name
+  start_ip_address = var.ip_address
+  end_ip_address = var.ip_address
 }
--- resources/variables.tf
+++ resources/variables.tf
@@ -1,10 +1,28 @@
 
 variable "prefix" {
     description = "The prefix used for all resources in this project"
+    type = string
     default = "azure-cms-app"
 }
 
 variable "location" {
     description = "The location of the resources in this project"
+    type = string
     default = "West US"
+}
+
+variable "db_name" {
+    description = "The name of the MySQL database"
+    type = string
+    default = "azure-cms"
+}
+
+variable "password" {
+    description = "Administrator password for MySQL server"
+    type = string
+}
+
+variable "ip_address" {
+    description = "Trusted ip address for accessing the MySQL server"
+    type = string
 }

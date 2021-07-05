e5125fd74a3bb4d0a237e8687fa578b4c0651f22
Max Boyko <maxboykomax@gmail.com>
2021-07-05 14:37:59 -0700

    Change database sku from business critical to basic

--- resources/main.tf
+++ resources/main.tf
@@ -27,9 +27,7 @@ resource "azurerm_mssql_database" "database" {
   server_id      = azurerm_mssql_server.database_server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
-  read_scale     = true
-  sku_name       = "BC_Gen5_2"
+  sku_name       = "Basic"
   zone_redundant = false
 }
 

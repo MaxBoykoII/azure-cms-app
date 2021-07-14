671c74e09149f79bd92c698489033d9219be842f
Max Boyko <maxboykomax@gmail.com>
2021-07-14 00:10:22 -0700

    Add output for primary storage access key

--- resources/output.tf
+++ resources/output.tf
@@ -5,3 +5,7 @@ output "cms_app_client_id" {
 output "cms_app_client_secret" {
   value = azuread_application_password.cms_app_pw.value
 }
+
+output "cms_images_blob_storage_key" {
+    value = azurerm_storage_account.storage_account.primary_access_key
+}

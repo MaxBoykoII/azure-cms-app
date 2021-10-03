bc3f18b6a8e42631e21ac84997023ec4ec8c02d5
Max Boyko <maxboykomax@gmail.com>
2021-10-02 17:50:27 -0700

    Fix formatting errors

--- resources/output.tf
+++ resources/output.tf
@@ -3,11 +3,11 @@ output "cms_app_client_id" {
 }
 
 output "cms_app_client_secret" {
-  value = azuread_application_password.cms_app_pw.value
+  value     = azuread_application_password.cms_app_pw.value
   sensitive = true
 }
 
 output "cms_images_blob_storage_key" {
-  value = azurerm_storage_account.storage_account.primary_access_key
+  value     = azurerm_storage_account.storage_account.primary_access_key
   sensitive = true
 }

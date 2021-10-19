1dc7bdd7f5416bf4acf70b3ce3d4cccc96f91bd4
Max Boyko <maxboykomax@gmail.com>
2021-10-18 17:37:01 -0700

    Log successful and unsuccess attempts to access the application

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -3,12 +3,18 @@ The flask application package.
 """
 
 from flask_session import Session
+from logging import StreamHandler
 
 from FlaskWebProject.login import login
 from FlaskWebProject.models import db
 from FlaskWebProject.views import app
 
-# TODO: Add any logging levels and handlers with app.logger
 Session(app)
+
 db.init_app(app)
 login.init_app(app)
+
+streamHandler = StreamHandler()
+app.logger.addHandler(streamHandler)
+
+
